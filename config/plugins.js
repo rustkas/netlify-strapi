9a690a2fd2f72850b2b26f52a309079da3ad4cb2
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-02-25 14:40:51 +0700

    add configuration settings

--- config/plugins.js
+++ config/plugins.js
@@ -15,6 +15,21 @@ module.exports = ({ env }) => ({
   'my-upload-folder-plugin': {
       enable: true,
       resolve: './src/plugins/my-upload-folder-plugin' 
-    }
+    },
+
+    // "netlify-deployments": {
+    //   enabled: true,
+    //   config: {
+    //     accessToken: env('NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN'),
+    //     sites: [
+    //       {
+    //         name: 'netlify-strapi',
+    //         id: "eb72454e-f2e9-4340-bdcc-7dff4782dd38",
+    //         buildHook: "https://api.netlify.com/build_hooks/63f9829254abe64181022804",
+    //         branch: 'main' // optional
+    //       }
+    //     ]
+    //   },
+    // },
   
 });
