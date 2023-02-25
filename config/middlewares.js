a7c6acc5d7c653e08fe25fb456606917e0d930d2
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-02-25 20:55:18 +0700

    Add updates

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,29 @@
-module.exports = ({ env }) => [
-  "strapi::errors",
-
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          'connect-src': ["'self'", 'http:', env("SUPABASE_URL")],
-          'img-src': ["'self'", 'data:', 'blob:', , env("SUPABASE_URL"), 'https://dl.airtable.com'],
-          'media-src': ["'self'", 'data:', 'blob:', env("SUPABASE_URL")],
+module.exports = ({ env }) => [  
+  "strapi::errors",  
+  {    
+    name: "strapi::security",    
+      config: {     
+        contentSecurityPolicy: {        
+          directives: {          
+            'connect-src': ["'self'", 'http:', env("SUPABASE_URL")],
+            'img-src': ["'self'", 'data:', 'blob:', , env("SUPABASE_URL"), 'https://dl.airtable.com'],
+            'media-src': ["'self'", 'data:', 'blob:', env("SUPABASE_URL")],
         },
       },
     },
   },
-  "strapi::cors",
   "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    // enabled: true,
+    config: {
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      origin: ['example.com', 'subdomain.example.com', 'someotherwebsite.org'],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      keepHeaderOnError: true,
+    },
+  },
+  
   "strapi::logger",
   "strapi::query",
   "strapi::body",
